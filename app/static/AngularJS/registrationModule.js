011c63a766c8642834ca9fcd8b46d71a28009b1a
agrijalva <agrijalva@bism.com.mx>
2017-08-08 12:16:19 -0500

    angular.filter

--- app/static/AngularJS/registrationModule.js
+++ app/static/AngularJS/registrationModule.js
@@ -6,7 +6,7 @@
 // -- Fecha: 
 // -- =============================================
 
-var registrationModule = angular.module("registrationModule", ["ngRoute", "LocalStorageModule", 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping', 'ui.grid.pinning', 'ui.grid.edit','ui.grid.moveColumns'])
+var registrationModule = angular.module("registrationModule", ["ngRoute", "LocalStorageModule", 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping', 'ui.grid.pinning', 'ui.grid.edit','ui.grid.moveColumns','angular.filter'])
 
     .config(function($routeProvider, $locationProvider) {
 
