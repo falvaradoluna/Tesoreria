36ceacc2aa0d360fd8cbab58d2652a9a2b4a1bdc
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-04-23 15:44:45 -0500

    conf

--- app/static/confront.js
+++ app/static/confront.js
@@ -1,4 +1,4 @@
 var global_settings = {
-   urlCORS: 'http://localhost:5200/'
-//    urlCORS: 'http://192.168.20.92:5200/'
+//    urlCORS: 'http://localhost:5200/'
+   urlCORS: 'http://192.168.20.92:5200/'
 }
