8cc1e5fc127d12d84235e0f5e450869822770758
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-07-06 17:26:26 -0500

    Revert "PermisosBugs"
    This reverts commit c5b81189907d4b478ac9cf3bbeac539d212c82b7.

--- app/static/AngularJS/login/loginController.js
+++ app/static/AngularJS/login/loginController.js
@@ -35,46 +35,36 @@ console.log($rootScope.currentEmployee)
         loginRepository.getPermisos(usuario).then(function(result) {
             console.log( 'result', result );
             localStorage.setItem( 'ShowBtns', JSON.stringify( result.data[1] ) );
-            if( result.data[1][0].Consulta == 1 ){
+            if (result.data.length > 0) {
                 $scope.login = result.data[0][0];
                 $scope.getEmpleado(usuario);
-                $rootScope.controlDepositosAcceso = 1;
-                $rootScope.conciliacionAccesso = 0;
-                alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0][0].nombreUsuario);
-                location.href = '/conciliacionInicioConsulta';
-                localStorageService.set('userData', $scope.login);
-            }else{
-                if (result.data.length > 0) {
-                    $scope.login = result.data[0][0];
-                    $scope.getEmpleado(usuario);
-                    if ($scope.login.idPerfil == 4) {
+                if ($scope.login.idPerfil == 4) {
+                    $rootScope.controlDepositosAcceso = 1;
+                    $rootScope.conciliacionAccesso = 0;
+                    alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0][0].nombreUsuario);
+                    location.href = '/conciliacionInicio';
+                    localStorageService.set('userData', $scope.login);
+                } else {
+                    if ($scope.login.idPerfil == 5) {
                         $rootScope.controlDepositosAcceso = 1;
-                        $rootScope.conciliacionAccesso = 0;
+                        $rootScope.conciliacionAccesso = 1;
                         alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0][0].nombreUsuario);
-                        location.href = '/conciliacionInicio';
+                        location.href = '/controlDepositos';
                         localStorageService.set('userData', $scope.login);
                     } else {
-                        if ($scope.login.idPerfil == 5) {
-                            $rootScope.controlDepositosAcceso = 1;
-                            $rootScope.conciliacionAccesso = 1;
-                            alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0][0].nombreUsuario);
-                            location.href = '/controlDepositos';
-                            localStorageService.set('userData', $scope.login);
-                        } else {
-                            $rootScope.controlDepositosAcceso = 0;
-                            $rootScope.conciliacionAccesso = 1;
-                            alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0][0].nombreUsuario);
-                            location.href = '/controlDepositos';
-                            localStorageService.set('userData', $scope.login);
-    
-                        }
+                        $rootScope.controlDepositosAcceso = 0;
+                        $rootScope.conciliacionAccesso = 1;
+                        alertFactory.warning('Bienvenido a Tesorería: ' + result.data[0][0].nombreUsuario);
+                        location.href = '/controlDepositos';
+                        localStorageService.set('userData', $scope.login);
+
                     }
-    
-                } else {
-                    alertFactory.info('Valide el usuario y/o contraseña');
                 }
+
+            } else {
+                alertFactory.info('Valide el usuario y/o contraseña');
             }
-            
+
         });
     }
 
